2198e002fa87ffb06327e94fe27343d9eafdacb4
Karma Patel <78253420+karmapatel@users.noreply.github.com>
2026-04-23 11:58:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,198 @@
+import random
+import pickle
+
+class Customer():
+    def __init__(self,accNo, name, phNo, branch, bal):
+        self.accNo = accNo
+        self.name = name
+        self.phNo = phNo
+        self.branch = branch
+        self.bal = bal
+
+def createAcc():
+    name = input('Enter Your Name: ')
+    phNo = input('Enter Your Phone No.: ')
+    branch = input('Enter Your Branch: ')
+    bal = int(input('Enter Intial Balance: '))
+    accNo = random.randint(10000,99999)
+    c = Customer(accNo, name, phNo, branch, bal)
+    try:
+        with open('customerData.pkl', 'ab') as fp:
+            pickle.dump(c,fp)
+    except:
+        print()
+        print('Error: 404 File Not Found')
+        print()
+    else:
+        print()
+        print(f'Your account is successfully generated with Account No. {accNo}')
+        print("Please Remember the Account No. as we don't have any provision to recover it  :)")
+        print()
+
+def checkBal():
+    acc = int(input('Enter Account Number: '))
+    try:
+        with open('customerData.pkl', 'rb') as fp:
+            while True:
+                obj = pickle.load(fp)
+                if obj.accNo == acc:
+                    print()
+                    print(f'Account Number: {obj.accNo}')
+                    print(f'Name: {obj.name}')
+                    print(f'Phone Number: {obj.phNo}')
+                    print(f'Branch: {obj.branch}')
+                    print(f'Balance: {obj.bal}')
+                    print()
+                    return
+    except:
+        print()
+        print('Error: 404 File Not Found')
+        print()
+    else:
+        print()
+        print('Account Not Found :(')
+        print()
+
+def withdraw():
+    acc = int(input('Enter Account Number: '))
+    data = []
+    try:
+        with open('customerData.pkl', 'rb') as fp:
+            found  = False
+            while True:
+                try:
+                    data.append(pickle.load(fp))
+                except EOFError:
+                    break
+            for c in data:
+                if c.accNo == acc:
+                    found  = True
+                    amt = int(input('Enter Amount to Withdraw: '))
+                    if c.bal >= amt:
+                        c.bal -= amt
+                        print()
+                        print(f'Withdraw Successfull !! Your current Balance is {c.bal}')
+                        print()
+                    else:
+                        print()
+                        print('Insufficient Balance')
+                        print()
+                    break
+        if found:
+            with open('customerData.pkl', 'wb') as fp:
+                for c in data:
+                    pickle.dump(c, fp)
+        else:
+            print()
+            print('Account Not Found :(')
+            print()
+    except:
+        print()
+        print('Error: 404 File Not Found')
+        print()
+
+def deposit():
+    acc = int(input('Enter Account Number: '))
+    data = []
+    try:
+        with open('customerData.pkl', 'rb') as fp:
+            found  = False
+            while True:
+                try:
+                    data.append(pickle.load(fp))
+                except EOFError:
+                    break
+            for c in data:
+                if c.accNo == acc:
+                    found  = True
+                    amt = int(input('Enter Amount to Deposit: '))
+                    c.bal += amt
+                    print()
+                    print(f'Deposit Successfull !! Your current Balance is {c.bal}')
+                    print()
+                    break
+        if found:
+            with open('customerData.pkl', 'wb') as fp:
+                for c in data:
+                    pickle.dump(c, fp)
+        else:
+            print()
+            print('Account Not Found :(')
+            print()
+    except:
+        print()
+        print('Error: 404 File Not Found')
+        print()
+
+def transfer():
+    sender = int(input("Enter Your Account Number: "))
+    target = int(input("Enter Recipient's Account Number: "))
+    data = []
+    s_found = False
+    t_found = False
+    try:
+        with open('customerData.pkl', 'rb') as fp:
+            while True:
+                try:
+                    data.append(pickle.load(fp))
+                except EOFError:
+                    break
+            for s in data:
+                if s.accNo == sender:
+                    s_found = True
+                    for t in data:
+                        if t.accNo == target:
+                            t_found = True
+                            amt = int(input('Enter Amount to Tranfer: '))
+                            if s.bal >= amt:
+                                s.bal -= amt
+                                t.bal += amt
+                                print()
+                                print(f'Transfer Successfull !! Your Current Balance is {s.bal}')
+                                print()
+                            else:
+                                print()
+                                print('Insufficient Balance')
+                                print()
+        if s_found and t_found:
+            with open('customerData.pkl', 'wb') as fp:
+                for c in data:
+                    pickle.dump(c, fp)
+        else:
+            print()
+            print(f'Either Account Not Found :( Please Check Again...')
+            print()
+    except:
+        print()
+        print('Error: 404 File Not Found')
+        print()
+
+
+def menu():
+    print('======= Welcome to Bank =======')
+    print('1. Create Account')
+    print('2. Check Balance')
+    print('3. Withdraw')
+    print('4. Deposit')
+    print('5. Transfer')
+    print('0. Exit')
+
+
+while True:
+    menu()
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        createAcc()
+    elif choice == 2:
+        checkBal()
+    elif choice == 3:
+        withdraw()
+    elif choice == 4:
+        deposit()
+    elif choice == 5:
+        transfer()
+    elif choice == 0:
+        print('Visit Again...')
+        break
+    else:
+        print('Invalid Choice !!')
